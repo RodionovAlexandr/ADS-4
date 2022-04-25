f710aed81702793354c9156fbb2cbebf70c141f1
RodionovAlexandr <99204870+RodionovAlexandr@users.noreply.github.com>
2022-04-25 14:10:39 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,116 @@
 // Copyright 2021 NNTU-CS
 int countPairs1(int *arr, int len, int value) {
+  int elem = 0;
+    int how = 0;
+    for (int i = 0; i <= len; i++) {
+        elem = *(arr + i);
+        for (int id = i + 1; id <= len; id++) {
+            if (elem + *(arr + id) == value)
+                how += 1;
+        }
+    }
+    return how;
   return 0;
 }
 int countPairs2(int *arr, int len, int value) {
+  int end = len - 1;
+  int beg = 0;
+  int elem1 = 0;
+  int elem2 = 0;
+  int how = 0;
+  while (beg < end) {
+      elem1 = *(arr + beg);
+      elem2 = *(arr + end);
+      if (elem1 + elem2 < value) {
+          beg += 1;
+          end = len - 1;
+      } else {
+          if (elem1 + elem2 == value) {
+              how += 1;
+              end -= 1;
+          }
+          else {
+              end--;
+          }
+      }
+  }
+  return how;
   return 0;
 }
 int countPairs3(int *arr, int len, int value) {
+  int elem1 = 0;
+        int elem2 =0;
+        int elem3 = 0;
+        int end = 0;
+        int beg = 0;
+        int c = 0;
+        int leftp = 0;
+        int rightp = len;
+        int how = 0;
+        for (int i = 0; i < len; i++) {
+
+            elem1 = *(arr + i);
+            elem2 = 0;
+            elem3 = 0;
+            rightp = len;
+            end = 0;
+            beg = 0;
+            c = 0;
+            while (elem2 + elem1 != value) {
+                c += 1;
+                if (*(arr + i) == -2147483648) break;
+                if (c == len) break;
+                if (value - elem1 > elem2) {
+                    leftp = end;
+                    end += (rightp - end) / 2;
+                    elem2 = *(arr + end);
+                    if (elem2 + elem1 == value) {
+                        *(arr + i) = -2147483648;
+                        elem3 = elem2;
+                        while (elem3 == elem2) {
+                            how += 1;
+                            beg += 1;
+                            elem3 = *(arr + end + beg);
+                        };
+                        elem3 = elem2;
+                        while (elem3 == elem2) {
+                            beg += 1;
+                            elem3 = *(arr + end - beg);
+                            if (elem3 == elem2) {
+                                how += 1;
+                            }
+                        }
+                    }
+                }
+                else {
+                    rightp = end;
+                    if (end == 1) {
+                        end = 0;
+                    }
+                    else {
+                        end -= (end - leftp) / 2;
+                    }
+                    elem2 = *(arr + end);
+                    if (elem2 + elem1 == value) {
+                        *(arr + i) = -2147483648;
+                        elem3 = elem2;
+                        while (elem3 == elem2) {
+                            how += 1;
+                            beg += 1;
+                            elem3 = *(arr + end + beg);
+                        }
+                        elem3 = elem2;
+                        while (elem3 == elem2) {
+                            beg += 1;
+                            elem3 = *(arr + end - beg);
+                            if (elem3 == elem2) {
+                                how += 1;
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        return how;
   return 0;
 }
